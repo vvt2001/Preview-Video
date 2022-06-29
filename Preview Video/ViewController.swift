f27e65c669b6206db9f92852ea446307a27a5a85
Vu Viet Thang <vuvietthang1508@gmail.com>
2022-06-29 08:23:47 +0700

    init project

---
+++ Preview Video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Preview Video
+//
+//  Created by Vũ Việt Thắng on 29/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
