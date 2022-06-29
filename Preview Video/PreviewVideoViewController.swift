50cf1d3872834366f0516666d3b0665d2e971171
Vu Viet Thang <vuvietthang1508@gmail.com>
2022-06-29 17:51:39 +0700

    thêm chức năng seek video

--- Preview Video/PreviewVideoViewController.swift
+++ Preview Video/PreviewVideoViewController.swift
@@ -17,48 +17,61 @@ class PreviewVideoViewController: UIViewController {
     @IBOutlet private weak var currentTimeLabel: UILabel!
     @IBOutlet private weak var totalDurationLabel: UILabel!
     @IBOutlet private weak var playAndPauseButton: UIButton!
+    @IBOutlet private weak var seekBarSlider: CustomSlider!
     @IBOutlet private var tapRecognizer: UITapGestureRecognizer!
     
     var videoPHAssets: [PHAsset]!
     private var currentAsset: PHAsset?
-    private var totalDuration: Int?
-    private var currentTime: Int = 0
+    private var totalDuration: Double = 0
+    private var currentTime: Double = 0
     private var isPlaying: Bool = true
     private var playerLayer = AVPlayerLayer()
+    private var player = AVPlayer()
     private var hideButtonItem: DispatchWorkItem?
-    
+    private var timeObserverToken: Any?
+
     @IBAction private func goBack(_ sender: UIButton){
         self.navigationController?.popViewController(animated: true)
-        playerLayer.removeFromSuperlayer()
     }
     
     @IBAction private func changeVideoTime(_ sender: UISlider){
-        
+        currentTime = totalDuration * Double(seekBarSlider.value/10000)
+        currentTimeLabel.text = getTimeString(time: currentTime)
+        player.seek(to: CMTime(seconds: Double(currentTime), preferredTimescale: CMTimeScale(NSEC_PER_SEC)))
+    }
+    
+    @IBAction private func handleSliderPanGesture(_ sender: UIGestureRecognizer){
+        switch sender.state{
+        case .began, .changed:
+            if isPlaying{
+                player.pause()
+                playAndPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            }
+            temporaryShowButton()
+        case .ended:
+            if isPlaying{
+                player.play()
+                playAndPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+            }
+        default:
+            break
+        }
+    }
+    
+    private func changeButtonState(){
+        let iconName = isPlaying ? "play" : "pause"
+        playAndPauseButton.setImage(UIImage(named: iconName), for: .normal)
     }
     
     @IBAction private func playAndPause(_ sender: UIButton){
         if isPlaying{
-            isPlaying = false
-            currentAsset?.getAVAsset(completionHandler: { asset in
-                //setup player
-                let playerItem = AVPlayerItem(asset: asset!)
-                let currentVideoPlayer = AVPlayer(playerItem: playerItem)
-                //play video
-                currentVideoPlayer.pause()
-            })
-            playAndPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            player.pause()
         }
         else{
-            isPlaying = true
-            currentAsset?.getAVAsset(completionHandler: { asset in
-                //setup player
-                let playerItem = AVPlayerItem(asset: asset!)
-                let currentVideoPlayer = AVPlayer(playerItem: playerItem)
-                //play video
-                currentVideoPlayer.play()
-            })
-            playAndPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+            player.play()
         }
+        changeButtonState()
+        isPlaying = !isPlaying
     }
     
     private func hideButton(){
@@ -80,7 +93,7 @@ class PreviewVideoViewController: UIViewController {
         hideButtonItem?.cancel()
     }
     
-    @IBAction private func handleTap(_ sender: UITapGestureRecognizer){
+    private func temporaryShowButton(){
         invalidateItem()
         UIView.transition(with: self.playAndPauseButton, duration: 0.5,
                           options: .transitionCrossDissolve,
@@ -90,23 +103,54 @@ class PreviewVideoViewController: UIViewController {
         hideButtonWithSchedule()
     }
     
+    @IBAction private func handleTap(_ sender: UITapGestureRecognizer){
+        temporaryShowButton()
+    }
+    
     private func setupVideoView(){
         let randomVideoAssets = self.videoPHAssets.randomElement()
         currentAsset = randomVideoAssets
         currentAsset?.getAVAsset(completionHandler: { asset in
             //setup player
             let playerItem = AVPlayerItem(asset: asset!)
-            let currentAsset = AVPlayer(playerItem: playerItem)
-            self.playerLayer = AVPlayerLayer(player: currentAsset)
+            self.player = AVPlayer(playerItem: playerItem)
+            self.playerLayer = AVPlayerLayer(player: self.player)
             
             //setup player layer layout
             self.playerLayer.frame = self.videoView.bounds
             self.playerLayer.videoGravity = .resizeAspectFill
             self.videoView.layer.insertSublayer(self.playerLayer, at: 0)
             
+            self.addPeriodicTimeObserver()
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(self.videoDidEnded), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player.currentItem)
+            
             //play video
-            currentAsset.play()
+            self.player.play()
         })
+    }
+    
+    @objc private func videoDidEnded(){
+        isPlaying = false
+        playAndPauseButton.setImage(UIImage(named: "play"), for: .normal)
+    }
+    
+    private func addPeriodicTimeObserver() {
+        let time = CMTime(seconds: 0.1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+
+        timeObserverToken = player.addPeriodicTimeObserver(forInterval: time, queue: .main, using: { time in
+            let currentTime = time.seconds
+            self.currentTime = currentTime
+            self.seekBarSlider.value = Float(currentTime / self.totalDuration) * 10000
+            self.currentTimeLabel.text = self.getTimeString(time: currentTime)
+        })
+    }
+    
+    private func removePeriodicTimeObserver() {
+        if let timeObserverToken = timeObserverToken {
+            player.removeTimeObserver(timeObserverToken)
+            self.timeObserverToken = nil
+        }
     }
     
     private func addBackgroundGradient(){
@@ -125,27 +169,20 @@ class PreviewVideoViewController: UIViewController {
         bottomGradientView.layer.insertSublayer(bottomGradient, at: 0)
     }
     
-    private func setVideoTimeLabel (){
-        // get video's duration
-        if currentAsset != nil{
-            let minutes = Int(currentAsset!.duration / 60)
-            let seconds = Int(currentAsset!.duration.truncatingRemainder(dividingBy: 60))
-            let minutesLabel = String(minutes)
-            var secondsLabel = String(seconds)
-            if seconds < 10{
-                secondsLabel = "0" + secondsLabel
-            }
-            totalDurationLabel.text =  minutesLabel + ":" + secondsLabel
-            totalDuration = Int(currentAsset!.duration)
-        }
-        else{
-            totalDurationLabel.text = ""
-        }
-    }
-    
     private func setupPlayAndPauseButton(){
         playAndPauseButton.layer.cornerRadius = 0.46 * playAndPauseButton.frame.width
         hideButtonWithSchedule()
+    }
+    
+    private func getTimeString(time: Double) -> String{
+        let minutes = Int(time / 60)
+        let seconds = Int(time.truncatingRemainder(dividingBy: 60))
+        let minutesLabel = String(minutes)
+        var secondsLabel = String(seconds)
+        if seconds < 10{
+            secondsLabel = "0" + secondsLabel
+        }
+        return minutesLabel + ":" + secondsLabel
     }
     
     override func viewDidLoad() {
@@ -157,8 +194,17 @@ class PreviewVideoViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupVideoView()
-        setVideoTimeLabel()
+        totalDuration = currentAsset!.duration
+        totalDurationLabel.text = getTimeString(time: totalDuration)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        invalidateItem()
+        removePeriodicTimeObserver()
+        player.pause()
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -169,4 +215,11 @@ class PreviewVideoViewController: UIViewController {
     }
     */
 
+}
+
+// MARK: - UIGestureRecognizerDelegate
+extension PreviewVideoViewController: UIGestureRecognizerDelegate{
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return true
+    }
 }
